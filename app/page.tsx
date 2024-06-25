773537bf7dfdfe6d81e3a36a5ea506e856ec130a
safarov-en <safarov.elkhan110@gmail.com>
2024-06-25 15:36:12 +0500

    Adding a button

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import { Button } from "@/components/Button/Button";
 import { Htag } from "@/components/Htag/Htag";
 
 export default function Home() {
   return (
     <div>
       <Htag tag="h1">Текст</Htag>
+      <Button appearance="primary">Кнопка</Button>
     </div>
   );
 }
