a4b367a564aa4a7e0a801027fc267a49e0306c38
safarov-en <safarov.elkhan110@gmail.com>
2024-06-26 12:06:57 +0500

    Adding the tag component

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,16 @@
 import { Button } from "@/components/Button/Button";
 import { Htag } from "@/components/Htag/Htag";
+import { Tag } from "@/components/Tag/Tag";
 
 export default function Home() {
   return (
     <div>
       <Htag tag="h1">Текст</Htag>
       <Button appearance="primary" arrow="right">Кнопка</Button>
+      <Tag size="s">Ghost</Tag>
+      <Tag size="m" color="red">Red</Tag>
+      <Tag size="s" color="green">Green</Tag>
+      <Tag color="primary">Green</Tag>
     </div>
   );
 }
