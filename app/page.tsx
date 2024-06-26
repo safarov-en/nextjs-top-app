52d5148790b6bf3f3cc4181ad6b4be8c63719256
safarov-en <safarov.elkhan110@gmail.com>
2024-06-26 11:35:50 +0500

    The paragraph component

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div>
       <Htag tag="h1">Текст</Htag>
-      <Button appearance="primary">Кнопка</Button>
+      <Button appearance="primary" arrow="right">Кнопка</Button>
     </div>
   );
 }
--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -1,16 +1,23 @@
 import styles from './Button.module.css'
 import { ButtonProps } from './Button.props'
 import cn from 'classnames'
+import Arrow from './arrow.svg'
 
-export const Button = ({appearance, children}: ButtonProps): JSX.Element => {
+export const Button = ({appearance, arrow = 'none', children, className, ...props}: ButtonProps): JSX.Element => {
     return (
         <button
-            className={cn(styles.button, {
+            className={cn(styles.button, className, {
                 [styles.primary]: appearance == 'primary',
                 [styles.ghost]: appearance == 'ghost'
             })}
+            {...props}
         >
             {children}
+            {arrow != 'none' && <span className={cn(styles.arrow, {
+                [styles.down]: arrow == 'down'
+            })}>
+                <Arrow />    
+            </span>}
         </button>
     )
 }
