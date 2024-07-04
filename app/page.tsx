83971720b402bd09300d5db5b32c2d2705b63160
safarov-en <safarov.elkhan110@gmail.com>
2024-07-04 12:37:01 +0500

    Rating change

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
+'use client'
+
 import { Button } from "@/components/Button/Button";
 import { Htag } from "@/components/Htag/Htag";
 import { Rating } from "@/components/Rating/Rating";
 import { Tag } from "@/components/Tag/Tag";
+import { useState } from "react";
 
 export default function Home() {
+  const [rating, setRating] = useState<number>(4)
   return (
     <div>
       <Htag tag="h1">Текст</Htag>
@@ -12,7 +16,7 @@ export default function Home() {
       <Tag size="m" color="red">Red</Tag>
       <Tag size="s" color="green">Green</Tag>
       <Tag color="primary">Green</Tag>
-      <Rating rating={4} />
+      <Rating rating={rating} isEditable setRating={setRating} />
     </div>
   );
 }
