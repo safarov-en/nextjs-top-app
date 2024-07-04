c6e12f6d59f70cc8eaf66dbccf154a0e542022bd
safarov-en <safarov.elkhan110@gmail.com>
2024-07-04 12:01:50 +0500

    Added rating component

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import { Button } from "@/components/Button/Button";
 import { Htag } from "@/components/Htag/Htag";
+import { Rating } from "@/components/Rating/Rating";
 import { Tag } from "@/components/Tag/Tag";
 
 export default function Home() {
@@ -11,6 +12,7 @@ export default function Home() {
       <Tag size="m" color="red">Red</Tag>
       <Tag size="s" color="green">Green</Tag>
       <Tag color="primary">Green</Tag>
+      <Rating rating={4} />
     </div>
   );
 }
