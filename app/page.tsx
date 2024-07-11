ce945f57b260cb5f388f7b856312d5572f60147b
safarov-en <safarov.elkhan110@gmail.com>
2024-07-11 16:11:47 +0500

    Adding a menu

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,6 @@
-import { getMenu } from "@/api/menu";
+import Menu from "@/components/Menu/Menu";
 
 export default async function Home() {
-  const menu = await getMenu(0)
-  return (
-    <div>
-      <ul>
-        {menu.map(m => (<li key={m._id.secondCategory}>{m._id.secondCategory}</li>))}
-      </ul>
-    </div>
-  );
+  return <></>
+    
 }
