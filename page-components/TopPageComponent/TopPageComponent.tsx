606b7b9d53d2962307589ec44338cfc06fff70d3
safarov-en <safarov.elkhan110@gmail.com>
2024-07-16 20:19:15 +0500

    Adding a card component

--- page-components/TopPageComponent/TopPageComponent.tsx
+++ page-components/TopPageComponent/TopPageComponent.tsx
@@ -1,7 +1,26 @@
+import { Htag } from "@/components/Htag/Htag"
 import { TopPageComponentProps } from "./TopPageComponent.props"
+import { Tag } from "@/components/Tag/Tag"
+import styles from './TopPageComponent.module.css'
 
 export const TopPageComponent = ({products, page, firstCategory}: TopPageComponentProps) => {
     return (
-        <>{products && products.length}</>
+        <div className={styles.wrapper}>
+            <div className={styles.title}>
+                <Htag tag='h1'>{page.title}</Htag>
+                {products && <Tag color='grey' size='m'>{products.length}</Tag>}
+                <span>Сортировка</span>
+            </div>
+            <div>
+                {products && products.map(p => (<div key={p._id}>{p.title}</div>))}
+            </div>
+            <div className={styles.hhTitle}>
+                <Htag tag='h2'>Вакансия - {page.category}</Htag>
+                <Tag color='red' size='m'>hh.ru</Tag>
+            </div>
+            <div className={styles.hh}>
+                
+            </div>
+        </div>
     )
 }
