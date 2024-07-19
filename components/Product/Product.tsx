6061578b04f4b510e11ab9663ea466c20c231b3e
safarov-en <safarov.elkhan110@gmail.com>
2024-07-19 16:55:32 +0500

    Adaptive layout

--- components/Product/Product.tsx
+++ components/Product/Product.tsx
@@ -42,7 +42,7 @@ export const Product = ({product, className, ...props}: ProductProps): JSX.Eleme
                     <div>{product.disadvantages}</div>
                 </div>}
             </div>
-            <Divider className={styles.hr} />
+            <Divider className={cn(styles.hr,styles.hr2)} />
             <div className={styles.action}>
                 <Button appearance="primary">Узнать подробнее</Button>
                 <Button appearance="ghost" arrow={'right'} className={styles.reviewButton}>Читать отзывы</Button>
