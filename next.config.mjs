f466f732213a992619385643d39d8dabab58c876
safarov-en <safarov.elkhan110@gmail.com>
2024-07-19 12:24:06 +0500

    Adding an image component

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    images: {
+        domains: ['old-images.hb.ru-msk.vkcs.cloud']
+    },
     webpack(config) {
         config.module.rules.push({
             test: /\.svg$/,
