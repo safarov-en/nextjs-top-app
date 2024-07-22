9afa4a300ed83aade84dbaf91e2001b3e09fd111
safarov-en <safarov.elkhan110@gmail.com>
2024-07-22 19:28:17 +0500

    Sending the form

--- components/ReviewForm/ReviewForm.tsx
+++ components/ReviewForm/ReviewForm.tsx
@@ -9,12 +9,34 @@ import { Textarea } from "../Textarea/Textarea"
 import { Button } from "../Button/Button"
 import CloseIcon from './close.svg'
 import { useForm, Controller } from "react-hook-form"
-import { IReviewForm } from "./ReviewForm.interface"
+import { IReviewForm, IReviewSentResponse } from "./ReviewForm.interface"
+import { API } from "@/app/api"
+import { useState } from "react"
 
 export const ReviewForm = ({productId, className, ...props}: ReviewFormProps): JSX.Element => {
-    const {register, control, handleSubmit, formState: {errors}} = useForm<IReviewForm>()
-    const onSubmit = (data: IReviewForm) => {
-
+    const {register, control, handleSubmit, formState: {errors}, reset} = useForm<IReviewForm>()
+    const [isSuccess, setIsSuccess] = useState<boolean>(false)
+    const [error, setError] = useState<string>()
+    const onSubmit = async (formData: IReviewForm) => {
+        try {
+            const res = await fetch(API.review.createDemo,  {
+                method: 'POST',
+                body: JSON.stringify({
+                    ...formData,
+                    productId
+                }),
+                headers: new Headers({ 'content-type': 'application/json' })
+            })
+            const data:IReviewSentResponse  = await res.json()
+            if(data.message) {
+                setIsSuccess(true)
+                reset()
+            } else {
+                setError('Что-то пошло не так')
+            }
+        } catch(e) {
+            setError(e.message)
+        }
     }
     return (
         <form onSubmit={handleSubmit(onSubmit)}>
@@ -40,13 +62,17 @@ export const ReviewForm = ({productId, className, ...props}: ReviewFormProps): J
                     <span className={styles.info}>* Перед публикацией отзыв пройдет предварительную модерацию и проверку</span>
                 </div>
             </div>
-            <div className={styles.success}>
+            {isSuccess && <div className={cn(styles.success, styles.panel)}>
                 <div className={styles.successTitle}>Ваш отзыв отправлен</div>
                 <div>
                     Спасибо, ваш отзыв будет опубликован после проверки.
                 </div>
-                <CloseIcon className={styles.close} />
-            </div>
+                <CloseIcon className={styles.close} onClick={() => setIsSuccess(false)} />
+            </div>}
+            {error && <div className={cn(styles.error, styles.panel)}>
+                Что-то пошло не так, попробуйте обновить страницу
+                <CloseIcon className={styles.close} onClick={() => setError(undefined)} />
+            </div>}
         </form>
     )
 }
