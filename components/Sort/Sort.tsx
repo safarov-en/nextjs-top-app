dc0b6c843ee4d49532e6f7192c165b269fb55349
safarov-en <safarov.elkhan110@gmail.com>
2024-07-26 11:38:58 +0500

    Adding aria attributes

--- components/Sort/Sort.tsx
+++ components/Sort/Sort.tsx
@@ -8,19 +8,26 @@ import SortIcon from './sort.svg'
 export const Sort = ({sort, setSort, className, ...props}: SortProps): JSX.Element => {
     return (
         <div className={cn(styles.sort, className)} {...props}>
+            <div className={styles.sortName} id="sort">Сортировка</div>
             <span
+                id="rating"
                 onClick={() => setSort(SortEnum.Rating)}
                 className={cn({
                     [styles.active]: sort == SortEnum.Rating
                 })}
+                aria-selected={sort == SortEnum.Rating}
+                aria-labelledby="sort rating"
             >
                 <SortIcon className={styles.sortIcon} />По рейтингу
             </span>
             <span
+                id="price"
                 onClick={() => setSort(SortEnum.Price)}
                 className={cn({
                     [styles.active]: sort == SortEnum.Price
                 })}
+                aria-selected={sort == SortEnum.Price}
+                aria-labelledby="sort price"
             >
                 <SortIcon className={styles.sortIcon} />По цене
             </span>
