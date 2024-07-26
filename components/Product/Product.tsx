ee75ba50655ceab871f95eb373ebef3feabc1933
safarov-en <safarov.elkhan110@gmail.com>
2024-07-26 13:30:50 +0500

    Sending the form

--- components/Product/Product.tsx
+++ components/Product/Product.tsx
@@ -69,6 +69,7 @@ export const Product = motion(forwardRef(({product, className, ...props}: Produc
                         arrow={isReviewOpened ? 'down' : 'right'}
                         className={styles.reviewButton}
                         onClick={() => setIsReviewOpened(!isReviewOpened)}
+                        aria-expanded={isReviewOpened}
                     >Читать отзывы</Button>
                 </div>
             </Card>
--- components/ReviewForm/ReviewForm.tsx
+++ components/ReviewForm/ReviewForm.tsx
@@ -14,7 +14,7 @@ import { API } from "@/app/api"
 import { useState } from "react"
 
 export const ReviewForm = ({productId, isOpened, className, ...props}: ReviewFormProps): JSX.Element => {
-    const {register, control, handleSubmit, formState: {errors}, reset} = useForm<IReviewForm>()
+    const {register, control, handleSubmit, formState: {errors}, reset, clearErrors} = useForm<IReviewForm>()
     const [isSuccess, setIsSuccess] = useState<boolean>(false)
     const [error, setError] = useState<string>()
     const onSubmit = async (formData: IReviewForm) => {
@@ -43,8 +43,8 @@ export const ReviewForm = ({productId, isOpened, className, ...props}: ReviewFor
             <div className={cn(styles.reviewForm, className)}
                 {...props}
             >
-                <Input {...register('name', {required: {value: true, message: 'Заполните имя'}})} placeholder="Имя" error={errors.name} tabIndex={isOpened ? 0 : -1} />
-                <Input {...register('title', {required: {value: true, message: 'Заполните заголовок'}})} placeholder="Заголовок отзыва" error={errors.title} className={styles.title} tabIndex={isOpened ? 0 : -1} />
+                <Input {...register('name', {required: {value: true, message: 'Заполните имя'}})} placeholder="Имя" error={errors.name} tabIndex={isOpened ? 0 : -1} aria-invalid={errors.name ? true : false} />
+                <Input {...register('title', {required: {value: true, message: 'Заполните заголовок'}})} placeholder="Заголовок отзыва" error={errors.title} className={styles.title} tabIndex={isOpened ? 0 : -1} aria-invalid={errors.title ? true : false} />
                 <div className={styles.rating}>
                     <span>Оценка:</span>
                     <Controller
@@ -56,9 +56,9 @@ export const ReviewForm = ({productId, isOpened, className, ...props}: ReviewFor
                         )}
                     />
                 </div>
-                <Textarea {...register('description', {required: {value: true, message: 'Заполните описание'}})} placeholder="Текст отзыва" className={styles.description} error={errors.description} tabIndex={isOpened ? 0 : -1} />
+                <Textarea {...register('description', {required: {value: true, message: 'Заполните описание'}})} placeholder="Текст отзыва" className={styles.description} error={errors.description} tabIndex={isOpened ? 0 : -1} aria-label='Текст отзыва' aria-invalid={errors.description ? true : false} />
                 <div className={styles.submit}>
-                    <Button appearance="primary" tabIndex={isOpened ? 0 : -1}>Отправить</Button>
+                    <Button appearance="primary" tabIndex={isOpened ? 0 : -1} onClick={() => clearErrors()}>Отправить</Button>
                     <span className={styles.info}>* Перед публикацией отзыв пройдет предварительную модерацию и проверку</span>
                 </div>
             </div>
